4c7461efb20e7cca0aa5c574bfb55dd5b7cb4675
wiji <wiji.permana@gmail.com>
2022-05-30 13:14:09 +0700

    Update

--- script.js
+++ script.js
@@ -37,6 +37,23 @@ for (let button of appButtons) {
 			return;
 		}
 
+		if (target.classList.contains("negative")) {
+			inverseNumber();
+			updateDisplay();
+			return;
+		}
+
+		if (target.classList.contains("equals")) {
+			performCalculation();
+			updateDisplay();
+			return;
+		}
+
+		if (target.classList.contains("operator")) {
+			handleOperator(target.innerText);
+			return;
+		}
+
 		inputDigit(target.innerText);
 		updateDisplay();
 	});
