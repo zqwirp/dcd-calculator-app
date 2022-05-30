66c3fc3b041d76391f2b866f33f623f9f7004706
wiji <wiji.permana@gmail.com>
2022-05-30 12:57:22 +0700

    Update

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
 const calculator = {
-  displayNumber: "0",
-  operator: null,
-  firstNumber: null,
-  waitingForSecondNumber: false,
+	displayNumber: "0",
+	operator: null,
+	firstNumber: null,
+	waitingForSecondNumber: false,
 };
 
 function updateDisplay() {
-  document.getElementById("output").innerText = calculator.displayNumber;
+	document.getElementById("output").innerText = calculator.displayNumber;
 }
 
 function clearCalculator() {
-  calculator.displayNumber = "0";
-  calculator.operator = null;
-  calculator.firstNumber = null;
-  calculator.waitingForSecondNumber = false;
+	calculator.displayNumber = "0";
+	calculator.operator = null;
+	calculator.firstNumber = null;
+	calculator.waitingForSecondNumber = false;
 }
 
 function inputDigit(digit) {
-  if (calculator.displayNumber === "0") {
-    calculator.displayNumber = digit;
-  } else {
-    calculator.displayNumber += digit;
-  }
+	if (calculator.displayNumber === "0") {
+		calculator.displayNumber = digit;
+	} else {
+		calculator.displayNumber += digit;
+	}
 }
 
 const app = document.getElementById("app");
 const appButtons = app.getElementsByTagName("button");
 
 for (let button of appButtons) {
-  button.addEventListener("click", function (event) {
-    const target = event.target;
+	button.addEventListener("click", function (event) {
+		const target = event.target;
 
-    inputDigit(target.innerText);
-    updateDisplay();
-  });
+		if (target.classList.contains("clear")) {
+			clearCalculator();
+			updateDisplay();
+			return;
+		}
+
+		inputDigit(target.innerText);
+		updateDisplay();
+	});
 }
